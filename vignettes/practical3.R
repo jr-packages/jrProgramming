c42abee2e24ed123041b92ab52213d92d178f146
Theo Roe <theo@jumpingrivers.com>
2019-08-12 11:56:20 +0100

    Adding the new movies data set (#4)
    * initial practical edits
    
    * initial practical edits
    
    * finalising new practical structure
    
    * makefile edit
    
    * removign duplicate vignette entry
    
    * changing vignette index entrys
    
    * tidying up solutions 5
    
    * small ggplot2 changes
    
    * adding movies data
    
    * addnig movies.Rd

---
+++ vignettes/practical3.R
@@ -0,0 +1,80 @@
+## ----echo=FALSE----------------------------
+# pfft old way to do things or what?
+# results = "hide"; echo = FALSE
+
+library(tufte)
+knitr::opts_chunk$set(results = "hide", echo = FALSE, message = FALSE)
+# gets rid of all the echo = echo, results = results
+# also makes it easier to be flexible about what shows where
+
+## ----echo=TRUE-----------------------------
+Fun4 = function(x) {
+  if(x==5) {
+    y = 0
+  } else {
+    y = 1
+  }
+  return(y)
+}
+
+## ------------------------------------------
+Fun4 = function(x) {
+  rtn_value = 0
+  if(x > 0) {
+    rtn_value = 1 
+  } else if (x < 0) {
+    rtn_value = -1
+  }
+  return(rtn_value)
+}
+
+## ---- echo = TRUE--------------------------
+dd = data.frame(x = rnorm(10),
+                y = rnorm(10),
+                z = rnorm(10))
+means = numeric(ncol(dd))
+for(i in seq_along(dd)) {
+  means[i] = mean(dd[, i])
+}
+
+## ---- echo = TRUE, message = TRUE----------
+x = 5
+message("The value of x is ", x)
+
+## ------------------------------------------
+means = numeric(ncol(dd))
+for(i in seq_along(dd)) {
+  m = mean(dd[,i])
+  if(m > 0.3){
+    message("Warning, the mean is greater than 0.3")
+  }
+  means[i] = mean(dd[, i])
+}
+
+## ------------------------------------------
+means = numeric(ncol(dd))
+for(i in seq_along(dd)) {
+  m = mean(dd[,i])
+  if(m > 0.3){
+    message("Warning, the mean of column ", i, " is greater than 0.3")
+  }
+  means[i] = mean(dd[, i])
+}
+
+## ------------------------------------------
+max_cols_limit = function(data, lim) {
+  means = numeric(ncol(dd))
+  for (i in seq_along(dd)) {
+    m = mean(dd[, i])
+    if (m > lim) {
+      message("Warning, the mean of column ", i, " is greater than ", lim)
+    }
+    means[i] = mean(dd[, i])
+  }
+  return(means)
+}
+max_cols_limit(dd, 0.2)
+
+## ----eval=FALSE, echo=TRUE-----------------
+#  vignette("solutions3", package = "jrProgramming")
+
---
+++ vignettes/solutions1.R
@@ -0,0 +1,51 @@
+## ----echo=FALSE----------------------------------------------------------
+library(tufte)
+# knitr::opts_chunk$set(results = "hide", echo = FALSE
+
+## ----results="hide", echo=TRUE-------------------------------------------
+total = 0
+for(i in 1:5) {
+  total = total + i
+}
+total
+
+## ------------------------------------------------------------------------
+total
+
+## ------------------------------------------------------------------------
+total = 0
+for(i in 1:20) {
+  total = total + (i + 1)
+}
+total
+
+## ------------------------------------------------------------------------
+total = 0
+for(j in -10:15) {
+  total = total + j
+}
+total
+
+## ------------------------------------------------------------------------
+sum(2:21)
+sum(-10:15)
+
+## ----echo=TRUE-----------------------------------------------------------
+dd = data.frame(x = rnorm(10), y = rnorm(10), z = rnorm(10))
+
+max_cols = numeric(ncol(dd))
+for(i in seq_along(dd)) {
+  max_cols[i] = max(dd[, i])
+}
+max_cols
+
+## ------------------------------------------------------------------------
+means = sds = numeric(ncol(dd))
+for(i in seq_along(dd)) {
+  means[i] = mean(dd[, i])
+  sds[i] = sd(dd[, i])
+}
+
+## ----eval=FALSE, echo=TRUE-----------------------------------------------
+#  vignette("solutions1", package = "jrProgramming")
+
